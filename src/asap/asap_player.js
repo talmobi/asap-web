e72a6e7d98e1240b82b94e86371b862d605f21b2
Christian Talmo <talmo.christian@gmail.com>
2018-04-02 21:30:23 +0300

    embedded binaryHttpRequest helper function dependency

--- src/asap/asap_player.js
+++ src/asap/asap_player.js
@@ -294,3 +294,50 @@ if ( typeof Cowbell 'object' && typeof Cowbell.Player === 'object' ) {
   }
 }
 
+/*
+  * binaryHttpRequest.js - download binary file from JavaScript
+  *
+  * Copyright (C) 2009-2012  Piotr Fusik
+  *
+  * This file is part of ASAP (Another Slight Atari Player),
+  * see http://asap.sourceforge.net
+  *
+  * ASAP is free software; you can redistribute it and/or modify it
+  * under the terms of the GNU General Public License as published
+  * by the Free Software Foundation; either version 2 of the License,
+  * or (at your option) any later version.
+  *
+  * ASAP is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty
+  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * See the GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with ASAP; if not, write to the Free Software Foundation, Inc.,
+  * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  */
+function binaryHttpRequest(url, onload)
+{
+  var req;
+  if (XMLHttpRequest)
+    req = new XMLHttpRequest(); // Chrome/Mozilla/Safari/IE7+
+  else
+    req = new ActiveXObject("Microsoft.XMLHTTP"); // IE6
+  req.open("GET", url, true);
+  if (req.overrideMimeType)
+    req.overrideMimeType("text/plain; charset=x-user-defined");
+  else
+    req.setRequestHeader("Accept-Charset", "x-user-defined");
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && (req.status == 200 || req.status == 0)) {
+      var result;
+      var response = req.responseText;
+      result = new Array(response.length);
+      for (var i = 0; i < response.length; i++) {
+        result[i] = response.charCodeAt(i) & 0xff;
+      }
+      onload(url, result);
+    }
+  };
+  req.send(null);
+}
